e309aa7b63ea82ce9ec39610d764ef2f96997520
Adrien Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2024-04-14 08:11:02 +0200

    Create CountAppApp.swift
    ContentVew added

---
+++ CountApp/CountAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountAppApp.swift
+//  CountApp
+//
+//  Created by Adrien on 13/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CountAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
